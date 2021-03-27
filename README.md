e8f9de3895909485bcbd4d7727407c3bf30e1299
JaedanC <jae.cal@hotmail.com>
2021-03-27 21:15:54 +1100

    Added documentation

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# SudokuSolver
-This python script solves Sudoku's in the terminal using the backtracking method. I did this for fun. There exist better solvers.
+# SudokuSolver 🧩
+
+This Python script solves Sudoku's in the terminal using the backtracking method. I did this for fun without external resources. This may not be the most efficient way to solve them.
+
+## Usage
+
+You add Sudoku's manually by adding them to the `board_data` list. The example's can be removed if you wish.
+
+## Data format
+
+![Sudoku](https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Sudoku_Puzzle_by_L2G-20050714_standardized_layout.svg/1200px-Sudoku_Puzzle_by_L2G-20050714_standardized_layout.svg.png)
+
+This Sudoku from wikipedia is input into my solver by reading each row and writing the number that appears. Write gaps as a `0`.
+
+For example, the above is:
+
+`"530070000600195000098000060800060003400803001700020006060000280000419005000080079"`
--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,5 +1,21 @@
 class SudokuSolver:
+    """The solver uses a list of Moves that wrap around empty tiles.
+    Tiles are looped through [1-9] until a valid number is found that
+    does not break the rules of the sudoku. This process begins at the
+    first tile. If a tiles loops through all 9 numbers, and they are
+    all invalid, then we go back a tile and increment it instead.
+
+    If this process can continue until the end successfully, then a
+    solution has been found. If the first tile loops through all 9
+    values then a solution is not possible at all.
+    """
     class Move:
+        """The Move class handles the blank tiles. It cycles through the
+        numbers [1-9] inside the tile.
+
+        Raises:
+            ValueError: If the tile hasn't got None for its number
+        """
         def __init__(self, tile):
             if tile.get_number() is not None:
                 raise ValueError("Moves can only be done to tiles with no number")
@@ -29,27 +45,25 @@ class SudokuSolver:
     def __init__(self, sudoku):
         self.sudoku = sudoku
         self.moves = []
-
+        
+        # The moves are premade and reset whenever not being used. This
+        # approach does not use a Stack which is likely a popular
+        # technique for other programmers solving this problem.
         for tile in sudoku:
             if tile.get_number() is None:
                 self.moves.append(self.Move(tile))
 
     def solve(self):
+        """Call solve to return the resultant finished Sudoku. This
+        method returns None if no solution can be found. It also
+        prints to the terminal an Error message.
         """
-        This is solved using the backtracking technique
-        """
-        if not self.sudoku.is_currectly_correct():
+        if not self.sudoku.is_currently_correct():
             print("The Sudoku is not possible to solve from initial values")
             return None
 
         counter = 0
-        # iterations = 0
         while True:
-        # while iterations < 4:
-            # if iterations % 10000 == 0:
-            #     print(iterations, self.sudoku)
-            # iterations += 1
-
             # Check if we found a solution, or can't find a solution
             if counter >= len(self.moves):
                 return self.sudoku
@@ -58,6 +72,8 @@ class SudokuSolver:
                 return None
             
             current_move = self.moves[counter]
+
+            # Try the next number for the tile
             if current_move.next_number():
                 row = current_move.get_tile().get_row()
                 col = current_move.get_tile().get_col()
@@ -68,8 +84,6 @@ class SudokuSolver:
                     self.sudoku.get_col(col),
                     self.sudoku.get_square(*square)
                 ]
-
-                # print(groups)
 
                 # Is this move legit?
                 results = [Sudoku.is_grouping_possible(group) for group in groups]
@@ -83,12 +97,18 @@ class SudokuSolver:
             else:
                 # This move is invalid with all numbers [1-9]
                 counter -= 1
-                continue
 
 
 class Sudoku:
     @staticmethod
     def is_grouping_possible(group):
+        """This static method checks to see whether the given group list has two 
+        (non-None) numbers that are the same. If so, return False. Otherwise,
+        return True.
+
+        Args:
+            group (list): The line to check (row, column, or, 3x3 square)
+        """
         numbers = list(map(lambda tile: tile.get_number(), group))
         numbers_no_none = list(filter(lambda n: n is not None, numbers))
         numbers_no_dups = set(numbers_no_none)
@@ -96,6 +116,13 @@ class Sudoku:
 
     class Tile:
         def __init__(self, row, col, number):
+            """Represents a tile in the Sudoku
+
+            Args:
+                row (int): Row index in the sudoku
+                col (int): Column index in the sudoku
+                number (int): Number associated with this tile
+            """
             self.row = row
             self.col = col
             if number == "0":
@@ -103,7 +130,6 @@ class Sudoku:
             else:
                 self.number = int(number)
             
-
         def __repr__(self):
             if self.number is None:
                 return " "
@@ -123,6 +149,16 @@ class Sudoku:
             return self.col
 
     def __init__(self, tile_data):
+        """Generates the tiles inside the Sudoku. Yes I used an inner class
+        for tiles. Don't hate me.
+
+        Args:
+            tile_data (str): 
+                See https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Sudoku_Puzzle_by_L2G-20050714_standardized_layout.svg/1200px-Sudoku_Puzzle_by_L2G-20050714_standardized_layout.svg.png)
+                Reading each row left to right and write the number that appears. Write gaps as a 0.
+                The above Sudoku is:
+                "530070000600195000098000060800060003400803001700020006060000280000419005000080079"
+        """
         self.tiles = [[None for __ in range(9)] for _ in range(9)]
 
         for row in range(9):
@@ -131,6 +167,8 @@ class Sudoku:
                 self.tiles[row][col] = self.Tile(row, col, number)
     
     def __str__(self):
+        """Shows the Sudoku board. Not very clean but gets the job done
+        """
         horizontal_line = "-" * ((3 * 5) + 4) + "\n"
         output = ""
         for i, row in enumerate(self.tiles):
@@ -161,12 +199,32 @@ class Sudoku:
         return tile
     
     def get_row(self, index):
+        """Returns the row at the specified index.
+
+        Args:
+            index (int): Must be [0-9)
+
+        Raises:
+            IndexError: If the row goes out of bounds
+        Returns:
+            list: The group of numbers in the row
+        """
         if (index < 0 or index >= 9):
             raise IndexError
         
         return self.tiles[index]
     
     def get_col(self, index):
+        """Returns the column at the specified index.
+
+        Args:
+            index (int): Must be [0-9)
+
+        Raises:
+            IndexError: If the column goes out of bounds
+        Returns:
+            list: The group of numbers in the column
+        """
         if (index < 0 or index >= 9):
             raise IndexError
         
@@ -176,6 +234,18 @@ class Sudoku:
         return col
     
     def get_square(self, row, col):
+        """Returns the 3x3 square at the specified row and column indices.
+
+        Args:
+            row (int): Must be [0-3)
+            col (int): Must be [0-3)
+
+        Raises:
+            IndexError: If the row or column goes out of bounds
+
+        Returns:
+            list: The group of numbers in the square
+        """
         if (row < 0 or row >= 3):
             raise IndexError
         if (col < 0 or col >= 3):
@@ -187,7 +257,18 @@ class Sudoku:
                 square.append(self.tiles[i][j])
         return square
     
-    def is_currectly_correct(self):
+    def is_currently_correct(self):
+        """Used to check if the current configuration of the board is valid.
+        If checking that a move is valid, it is faster to not check the entire
+        board, but only the groups that are affected by the move.
+
+        A return of True does not mean that the Sudoku is solved. It also does
+        not mean that a solution exists. It should only be used to check that
+        the input Sudoku is legitimate.
+
+        Returns:
+            bool: True if the board is in a naively solvable configuration.
+        """
         all_groups = []
         for i in range(9):
             all_groups.append(game.get_row(i))
